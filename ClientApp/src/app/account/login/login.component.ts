ba31934a39987064e76696069b5d838ad177ee74
claudengounou <claudengounou@gmail.com>
2023-10-03 13:15:08 +0100

    Section 04. User persistance in angular

--- ClientApp/src/app/account/login/login.component.ts
+++ ClientApp/src/app/account/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SharedService } from 'src/app/shared/shared.service';
+import { take } from 'rxjs';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +15,30 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
   submitted = false;
   errorMessages: string[] = [];
+  returnUrl: string | null = null;
 
   constructor(
     private accountService: AccountService,
     private formBuilder: FormBuilder,
-    private router: Router
-  ) {}
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.accountService.user$.pipe(take(1)).subscribe({
+      next: (user: User | null ) => {
+        if(user){
+          this.router.navigateByUrl('/')
+        } else {
+          this.activatedRoute.queryParamMap.subscribe({
+            next: (params: any) => {
+              if(params){
+                this.returnUrl = params.get('returnUrl');
+              }
+            }
+          })
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -35,9 +55,14 @@ export class LoginComponent implements OnInit {
     this.submitted = true;
     this.errorMessages = [];
 
-    //if (this.loginForm.valid) {
+    if (this.loginForm.valid) {
       this.accountService.login(this.loginForm.value).subscribe({
         next: (response: any) => {
+          if(this.returnUrl) {
+            this.router.navigateByUrl(this.returnUrl);
+          } else {
+            this.router.navigateByUrl('/');
+          }
           
         },
         error: (error) => {
@@ -49,7 +74,7 @@ export class LoginComponent implements OnInit {
           
         },
       });
-    //}
+    }
 
   }
 }
--- ClientApp/src/app/shared/models/register.ts
+++ ClientApp/src/app/shared/models/register.ts
@@ -1,7 +1,6 @@
-export interface Register{
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-
+export interface Register {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
 }
